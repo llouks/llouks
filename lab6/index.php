ce3528c8e18a490abc3e8c27e627f5f78df6d7df
Logan Louks <llouks@csumb.edu>
2018-04-01 04:07:17 +0000

    lab6 heroku deploy

--- lab6/index.php
+++ lab6/index.php
@@ -1,7 +1,7 @@
 
 <?php
 
-    include '../dbConnection.php';
+    include 'dbConnection.php';
     
     $conn = getDatabaseConnection("ottermart");
 
@@ -107,7 +107,7 @@
         }
         form {
             text-align:center;
-             
+             border: 4px solid black;
         }
         h1 {
             color: red;
